4fb35fcf2912d3c15d510154d2fdca7bf20f899f
AndrUsM <oksud22@gmail.com>
2020-03-16 02:16:32 +0200

    icon, event handler, list of events

---
+++ src/components/universal_templates/buttons/active_icon/Icon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconPropsType,  } from './types';
+
+export default function Icon(props: IconPropsType) {
+    return (
+        <FontAwesomeIcon
+            icon={props.icon}
+            className={props.className || ""}
+            style={{
+                color: props.color || "",
+                background: props.background || ""
+            }}
+        />
+    )
+}
