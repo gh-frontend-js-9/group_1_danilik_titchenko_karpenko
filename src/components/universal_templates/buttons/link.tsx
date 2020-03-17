771c035272ee7922654be6432e3e06c87a339572
AndrUsM <oksud22@gmail.com>
2020-03-18 00:42:24 +0200

    create ButtonLink

---
+++ src/components/universal_templates/buttons/link.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function GeneralLinkElement (
+    props: {
+        href: string,
+        className?: string,
+        style?: React.CSSProperties,
+        children?: any
+    }) {
+    return (
+        <div className = "center-vertical">
+            <Link
+                to={props.href}
+                style={props.style}
+                className={`${props.className}`}>
+                {props.children}
+            </Link>
+        </div>
+    )
+}
