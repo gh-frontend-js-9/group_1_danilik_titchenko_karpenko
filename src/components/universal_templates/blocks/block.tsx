57b30d71421ada47a7ce544697a664f9a2be3648
AndrUsM <oksud22@gmail.com>
2020-03-18 17:48:16 +0200

    update blocks

--- src/components/universal_templates/blocks/block.tsx
+++ src/components/universal_templates/blocks/block.tsx
@@ -4,9 +4,9 @@ export default function (props: {
     children?: Array<any>
 }) {
     return (
-        <section className = "block__wrapper">
-            {props.children?.map((value, index:number) => {
-                return <article key = { index } className = "block__element">{value}</article>
+        <section className="block__wrapper">
+            {props.children?.map((value, index: number) => {
+                return <article key={index} className="block__element">{value}</article>
             })}
         </section>
     )
