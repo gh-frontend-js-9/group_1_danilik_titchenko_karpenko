cce98ca09cdde1fe15dd9cf215a4ec724dc92e6e
Yaroslav Danilik <yarosalvdaniliK@gmail.com>
2020-03-24 12:25:40 +0200

    second merge

--- src/pages/professional_blog.tsx
+++ src/pages/professional_blog.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import 'normalize.css';
+import Header from '../components/header/Header';
 import ArticleBlock from '../components/professional_blog/article_block';
 import ArticleImageBlock from '../components/professional_blog/reading_list';
+import Footer from '../components/footer/Footer';
+
+
 export default function(){
     return (
         <>
+        <Header></Header>
             <ArticleBlock/>
             <ArticleImageBlock/>
+            <Footer></Footer>
         </>
     )
 }
