1f2e7a60fbc9aed1a2f9cdf61e52611a0b278d08
AndrUsM <oksud22@gmail.com>
2020-03-24 12:17:57 +0200

    combine components into a page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import 'normalize.css';
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import Link from './components/universal_templates/buttons/link'
+import SiteLogo, { }  from './components/universal_templates/buttons/site_logo'
+import {SocialIconGray, SocialIconWhite} from './components/universal_templates/buttons/social_icons'
+
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faAddressBook } from '@fortawesome/free-solid-svg-icons';
 // https://www.npmjs.com/package/react-responsive-carousel
-// for carousel 
+// for carousel
 
 function App() {
-  return (
-    <>
-      <Header></Header>
-      <Footer></Footer>
-    </>
-  );
+    return (
+        <div>
+            <SiteLogo color = "gray"></SiteLogo>
+            <SiteLogo color = "main"></SiteLogo>
+
+            <Link href = "#">Link#1</Link>
+            
+            <SocialIconGray href = "" icon = { faFacebook }></SocialIconGray>
+            <SocialIconGray icon = { faFacebook }></SocialIconGray>
+        </div>
+    )
 }
 
 export default App;
--- src/pages/professional_blog.tsx
+++ src/pages/professional_blog.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ArticleBlock from '../components/professional_blog/article_block';
 import ArticleImageBlock from '../components/professional_blog/reading_list';
-export default function(){
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+import HeaderCarousel from '../components/professional_blog/header_carousel';
+import Section from '../components/universal_templates/blocks/section';
+
+export default function () {
     return (
         <>
-            <ArticleBlock/>
-            <ArticleImageBlock/>
+            <Section>
+                <Header />
+                <HeaderCarousel />
+            </Section>
+            <Section className = "block__body">
+                <ArticleBlock />
+                <ArticleImageBlock />
+            </Section>
+            <Footer />
         </>
     )
 }
