6f9ed1628c974c4bebafcaf789198224d41f71b7
-=EL=- <IT0671581471@gmail.com>
2020-03-25 21:16:39 +0200

    section header no burger

--- src/pages/professional_blog.tsx
+++ src/pages/professional_blog.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import 'normalize.css';
-import Header from '../components/header/Header';
-import Footer from '../components/footer/Footer';
 import ArticleBlock from '../components/professional_blog/article_block';
 import ArticleImageBlock from '../components/professional_blog/reading_list';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
 import HeaderCarousel from '../components/professional_blog/header_carousel';
 import Section from '../components/universal_templates/blocks/section';
 
@@ -14,7 +13,7 @@ export default function () {
                 <Header />
                 <HeaderCarousel />
             </Section>
-            <Section className="block__body">
+            <Section className = "block__body">
                 <ArticleBlock />
                 <ArticleImageBlock />
             </Section>
