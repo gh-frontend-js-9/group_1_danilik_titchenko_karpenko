f7134df56bfa897503c03869727ca250a2493dce
Yaroslav Danilik <yarosalvdaniliK@gmail.com>
2020-03-29 23:50:35 +0300

    rename js in tsx

--- src/pages/professional_blog.tsx
+++ src/pages/professional_blog.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ArticleBlock from '../components/professional_blog/article_block';
 import ArticleImageBlock from '../components/professional_blog/reading_list';
+import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import HeaderCarousel from '../components/professional_blog/header_carousel';
 import Section from '../components/universal_templates/blocks/section';
@@ -9,8 +10,8 @@ export default function () {
     return (
         <>
             <Section>
-                <Header />
-                <HeaderCarousel />
+                <Header></Header>
+                <HeaderCarousel/>
             </Section>
             <Section className = "block__body">
                 <ArticleBlock />
