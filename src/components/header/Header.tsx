3133a5724e8bb557dc1f0e7b5402829ce1eb9359
AndrUsM <oksud22@gmail.com>
2020-04-02 00:54:27 +0300

    add side-menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,35 +1,61 @@
-import React from 'react';
-import SiteLogo, { } from '../universal_templates/buttons/site_logo';
+import React, { createRef, RefObject } from 'react';
+import SiteLogo from '../universal_templates/buttons/site_logo';
 import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { SocialIconWhite } from '../universal_templates/buttons/social_icons';
-import { social_links_data } from '../data';
+import { social_links_data, nav_links_data } from '../data';
 import './Header.scss';
+import SideMenu from "../universal_templates/menu";
+import NavLink from '../universal_templates/buttons/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default () => (
-    <header className="header">
-        <div className="header-social-block">
-            {
-                social_links_data.map((value, index) => {
-                    return (
-                        <SocialIconWhite 
-                            key={index} 
-                            href={value.href} 
-                            icon={value.icon} 
+export default function () {
+    const nav_button_ref: RefObject<HTMLButtonElement> = createRef();
+    return (
+        <header className="header">
+            <div className="header-social-block">
+                {
+                    social_links_data.map((value, index) => {
+                        return (
+                            <SocialIconWhite
+                                key={index}
+                                href={value.href}
+                                icon={value.icon}
+                            />
+                        )
+                    })
+                }
+            </div>
+            <div className="header-logo">
+                <SiteLogo color="main"></SiteLogo>
+            </div>
+            <div className="header-menu-block">
+                <div className="menu-block">
+                    <button
+                        className="button_social-white"
+                        ref={nav_button_ref}>
+                        <FontAwesomeIcon
+                            icon={faBars}
                         />
-                    )
-                })
-            }
-        </div>
-        <div className="header-logo">
-            <SiteLogo color="main"></SiteLogo>
-        </div>
-        <div className="header-menu-block">
-            <div className="menu-block">
-                <SocialIconWhite icon={faBars}></SocialIconWhite>
+                    </button>
+
+                </div>
+                <div className="search-block">
+                    <SocialIconWhite icon={faSearch}></SocialIconWhite>
+                </div>
             </div>
-            <div className="search-block">
-                <SocialIconWhite icon={faSearch}></SocialIconWhite>
-            </div>
-        </div>
-    </header>
-)
+            <SideMenu button_ref={nav_button_ref}>
+                {
+                    nav_links_data.map((value, index) => {
+                        return (
+                            <NavLink 
+                                className = "list__element list__element-base"
+                                href={value.href}>
+                                    {value.text}
+                            </NavLink>
+                        )
+                    })
+                }
+            </SideMenu>
+        </header>
+    )
+}
