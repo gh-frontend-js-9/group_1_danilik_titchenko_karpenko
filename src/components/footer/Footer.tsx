dceeb9bb56ede3d9cf30694e4850a4be81e86ffb
AndrUsM <oksud22@gmail.com>
2020-04-02 00:28:07 +0300

    finalized components

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import SiteLogo, { }  from '../universal_templates/buttons/site_logo';
+import SiteLogo, { } from '../universal_templates/buttons/site_logo';
 import { faFacebook, faTwitter, faInstagram, faVk, faPinterest } from '@fortawesome/free-brands-svg-icons';
 // import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { SocialIconGray } from '../universal_templates/buttons/social_icons';
 import './Footer.scss';
+import { social_links_data } from '../data';
 
 
 
@@ -11,11 +12,17 @@ export default () => (
     <footer className="footer">
         <SiteLogo color="gray"></SiteLogo>
         <div className="footer-social-block">
-            <SocialIconGray icon={ faFacebook }></SocialIconGray>
-            <SocialIconGray icon={ faTwitter }></SocialIconGray>
-            <SocialIconGray icon={ faInstagram }></SocialIconGray>
-            <SocialIconGray icon={ faVk }></SocialIconGray>
-            <SocialIconGray icon={ faPinterest }></SocialIconGray>
+            {
+                social_links_data.map((value, index) => {
+                    return (
+                        <SocialIconGray
+                            key={index}
+                            icon={value.icon}
+                            href={value.href}
+                        />
+                    )
+                })
+            }
         </div>
         <div className="footer-copyright">
             <p className="footer-copyright__text">All Rights Reserved 2018 BehindMars</p>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
-import SiteLogo, { }  from '../universal_templates/buttons/site_logo';
-import { faFacebook, faTwitter, faInstagram, faVk, faPinterest } from '@fortawesome/free-brands-svg-icons';
+import SiteLogo, { } from '../universal_templates/buttons/site_logo';
 import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { SocialIconWhite } from '../universal_templates/buttons/social_icons';
+import { social_links_data } from '../data';
 import './Header.scss';
 
 export default () => (
     <header className="header">
         <div className="header-social-block">
-            <SocialIconWhite icon={ faFacebook }></SocialIconWhite>
-            <SocialIconWhite icon={ faTwitter }></SocialIconWhite>
-            <SocialIconWhite icon={ faInstagram }></SocialIconWhite>
-            <SocialIconWhite icon={ faVk }></SocialIconWhite>
-            <SocialIconWhite icon={ faPinterest }></SocialIconWhite>
+            {
+                social_links_data.map((value, index) => {
+                    return (
+                        <SocialIconWhite 
+                            key={index} 
+                            href={value.href} 
+                            icon={value.icon} 
+                        />
+                    )
+                })
+            }
         </div>
         <div className="header-logo">
-        <SiteLogo color="main"></SiteLogo>
+            <SiteLogo color="main"></SiteLogo>
         </div>
         <div className="header-menu-block">
             <div className="menu-block">
-                <SocialIconWhite icon={ faBars }></SocialIconWhite>
+                <SocialIconWhite icon={faBars}></SocialIconWhite>
             </div>
             <div className="search-block">
-                <SocialIconWhite icon={ faSearch }></SocialIconWhite>
+                <SocialIconWhite icon={faSearch}></SocialIconWhite>
             </div>
         </div>
     </header>
