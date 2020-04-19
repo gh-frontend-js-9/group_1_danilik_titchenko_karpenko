abf02c03392d36789f2f9d91f6612ab8eda393ab
AndrUsM <oksud22@gmail.com>
2020-04-19 23:29:31 +0300

    move folder

--- src/components/pages_components/header/NavHeader.tsx
+++ src/components/pages_components/header/NavHeader.tsx
@@ -2,7 +2,7 @@ import React, { createRef, RefObject } from 'react';
 import SiteLogo from '../../universal_templates/buttons/site_logo';
 import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { SocialIconGray } from '../../universal_templates/buttons/social_icons';
-import { nav_links_data } from '../../data';
+import { nav_links_data } from '../data';
 import NavLink from '../../universal_templates/buttons/link';
 import './Header.scss';
 import SideMenu from "../../universal_templates/menu";
