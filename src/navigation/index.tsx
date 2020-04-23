1a5ab58fe69b8e16f14334b39b2dacd4ec96f4c9
Yaroslav Danilik <yarosalvdaniliK@gmail.com>
2020-04-23 12:27:05 +0300

    delete some pages

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -6,9 +6,6 @@ import Popular from '../pages/popular';
 import BusinessBlog from '../pages/business_blog';
 import PersonalBlog from '../pages/personal_blog';
 import NewPage from '../pages/new';
-import Subscribe from '../pages/subscribe';
-import List from '../pages/list';
-import Topics from '../pages/topics';
 
 export default function NavigationPreloadManager() {
     return (
@@ -19,9 +16,6 @@ export default function NavigationPreloadManager() {
                 <Route path="/business" component={BusinessBlog} />
                 <Route path="/personal" component={PersonalBlog} />
                 <Route path="/new" component={NewPage}/>
-                <Route path="/subscribe" component={Subscribe}/>
-                <Route path="/list" component={List}/>
-                <Route path="/topics" component={Topics}/>
             </Switch>
         </BrowserRouter>
     )
